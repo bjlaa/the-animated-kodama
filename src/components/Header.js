ebd2ef0dacc55e5f5fc1049817a6e07411812043
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-11-14 18:20:39 +0100

    feat: Installed GSAP+ started animations headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { TimelineLite, TweenMax } from 'gsap';
 
-const Header = () => {
-  return (
-    <div className="header">
-      <h1>The Animation-Man</h1>
-      <h2>by Benjamin Arias</h2>      
-    </div>
-  );
-};
+class Header extends Component {
+  componentDidMount() {
+    const tl = new TimelineLite();
+
+    const hOne = this.hOne;
+    //const tween = TweenMax.to(hOne, 2, {color: 'red'});
+    const hTwo = this.hTwo;
+
+    tl
+    .to(hTwo, 0, {
+      display: 'none',
+      opacity: 0,
+    })
+    .from(hOne, 3, {
+      css: { 
+        transform: 'translate(-50px, 100px)',
+        opacity: 0,
+      },
+      ease: Elastic.easeInOut,
+    })
+    .to(hOne, 1.5, {
+      css: { 
+        transform: 'translate(150px, 300px)',
+        opacity: 0,
+        display: 'none',
+      },
+      ease: Elastic.easeInOut,
+    })
+    .to(hTwo, 0, {
+      css: { 
+        display: 'initial',
+      },
+    })
+    .to(hTwo, 3, {
+      css: { 
+        opacity: 1,
+      },
+      ease: Elastic.easeInOut,
+    }, 4.5);
+  }
+  render() {
+    return (
+      <div className="header">
+        <h1 ref={(hOne) => this.hOne = hOne}>The Animation-Man</h1>
+        <h2 ref={(hTwo) => this.hTwo = hTwo}>by Benjamin Arias</h2>      
+      </div>
+    );    
+  }
+}
 
 export default Header;
